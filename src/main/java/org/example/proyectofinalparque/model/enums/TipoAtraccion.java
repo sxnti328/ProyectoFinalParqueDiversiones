91552abbf5a9b47de657337addc97f561e335e73
juancami <juanc.restrepoa1@uqvirtual.edu.co>
2026-05-21 00:01:20 -0500

    .

--- src/main/java/org/example/proyectofinalparque/model/enums/TipoAtraccion.java
+++ src/main/java/org/example/proyectofinalparque/model/enums/TipoAtraccion.java
@@ -1,10 +1,7 @@
 package org.example.proyectofinalparque.model.enums;
 
 public enum TipoAtraccion {
-    MONTANNA_RUSA,
     MECANICA_ALTURA,
-    CARRUSEL,
-    RUEDA_CHICAGO,
     ACUATICA,
     INFANTIL,
     FAMILIAR,
